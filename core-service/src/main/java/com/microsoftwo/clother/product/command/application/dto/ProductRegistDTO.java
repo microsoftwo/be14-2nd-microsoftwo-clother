8e9824379128c2111b51f0b1b0d6a099e262629d
Sumin <emily9949@naver.com>
2025-03-25 06:36:57 +0900

    [feat] 유저의 상품 등록 신청 기능 추가

--- core-service/src/main/java/com/microsoftwo/clother/product/command/application/dto/ProductRegistDTO.java
+++ core-service/src/main/java/com/microsoftwo/clother/product/command/application/dto/ProductRegistDTO.java
@@ -1,6 +1,9 @@
 package com.microsoftwo.clother.product.command.application.dto;
 
+import com.fasterxml.jackson.annotation.JsonFormat;
 import lombok.*;
+
+import java.util.Date;
 
 @AllArgsConstructor
 @NoArgsConstructor
@@ -8,4 +11,10 @@ import lombok.*;
 @Setter
 @ToString
 public class ProductRegistDTO {
+    private String brandName;
+    private String name;
+    private String productLink;
+    @JsonFormat(shape = JsonFormat.Shape.STRING, pattern = "yyyy-MM-dd HH:mm:ss", timezone = "Asia/Seoul")
+    private Date createdAt = new Date(); // 기본값 현재 시간이 들어가게
+    private int userId;
 }
--- core-service/src/main/java/com/microsoftwo/clother/product/command/domain/repository/ProductRepository.java
+++ core-service/src/main/java/com/microsoftwo/clother/product/command/domain/repository/ProductRepository.java
@@ -1,8 +1,10 @@
 package com.microsoftwo.clother.product.command.domain.repository;
 
+import com.microsoftwo.clother.product.command.domain.aggregate.ProductRegist;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 @Repository
-public interface ProductRepository extends JpaRepository<Product, > {
+public interface ProductRepository extends JpaRepository<ProductRegist, Integer> {
+
 }
