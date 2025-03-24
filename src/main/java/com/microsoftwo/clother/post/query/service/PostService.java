4dba3ac9fcf5947e891fc62e7f56c77e35fde2a0
cxzaqq <roy1228@naver.com>
2025-03-24 12:07:39 +0900

    [refactor] 불필요한 코드 제거

--- src/main/java/com/microsoftwo/clother/post/query/service/PostService.java
+++ src/main/java/com/microsoftwo/clother/post/query/service/PostService.java
@@ -1,7 +1,6 @@
 package com.microsoftwo.clother.post.query.service;
 
 import com.microsoftwo.clother.post.query.dto.PostAndHairTagDTO;
-import com.microsoftwo.clother.post.query.dto.TestPostDTO;
 
 public interface PostService {
 
