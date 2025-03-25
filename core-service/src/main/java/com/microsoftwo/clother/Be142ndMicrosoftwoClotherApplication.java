0a0a8794fe7b77cf0b23b78e4cb12025f3f1102f
cxzaqq <roy1228@naver.com>
2025-03-25 12:51:38 +0900

    [chore] 필요 설정 추가

--- core-service/src/main/java/com/microsoftwo/clother/Be142ndMicrosoftwoClotherApplication.java
+++ core-service/src/main/java/com/microsoftwo/clother/Be142ndMicrosoftwoClotherApplication.java
@@ -2,8 +2,10 @@ package com.microsoftwo.clother;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 
 @SpringBootApplication(scanBasePackages = "com.microsoftwo.clother")
+@EnableDiscoveryClient
 public class Be142ndMicrosoftwoClotherApplication {
 
     public static void main(String[] args) {
