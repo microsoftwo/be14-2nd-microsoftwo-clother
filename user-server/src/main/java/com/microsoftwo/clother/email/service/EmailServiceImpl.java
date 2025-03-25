4b4f6f7e107c174db52d37c55676b03b17a35274
mmmv41 <tlsalsrud5068@daum.net>
2025-03-25 16:37:38 +0900

    [refactor] 이메일 인증 코드 부분 수정

--- user-server/src/main/java/com/microsoftwo/clother/email/service/EmailServiceImpl.java
+++ user-server/src/main/java/com/microsoftwo/clother/email/service/EmailServiceImpl.java
@@ -5,7 +5,6 @@ import com.microsoftwo.clother.user.service.UserService;
 import java.io.IOException;
 import java.nio.file.Files;
 import java.nio.file.Paths;
-import java.util.Random;
 import javax.mail.MessagingException;
 import javax.mail.internet.MimeMessage;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -32,13 +31,7 @@ public class EmailServiceImpl implements EmailService {
 
     // 임의의 6자리 양수를 반환 (이메일 인증 코드)
     public void makeRandomNumber() {
-        Random r = new Random();
-        String randomNumber = "";
-        for (int i = 0; i < 6; i++) {
-            randomNumber += Integer.toString(r.nextInt(10));
-        }
-
-        authNumber = Integer.parseInt(randomNumber);
+        authNumber = (int) (Math.random() * 900000) + 100000;
     }
 
     @Override
