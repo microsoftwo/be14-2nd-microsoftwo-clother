f9469fa548080bebc902edfdbf361f3071e62d29
mmmv41 <tlsalsrud5068@daum.net>
2025-03-31 21:25:10 +0900

    [feat] 삭제된 부분 수정

--- user-server/src/main/java/com/microsoftwo/clother/security/service/AuthService.java
+++ user-server/src/main/java/com/microsoftwo/clother/security/service/AuthService.java
@@ -6,6 +6,7 @@ import com.microsoftwo.clother.security.jwt.TokenProvider;
 import com.microsoftwo.clother.security.repository.RefreshTokenRepository;
 import com.microsoftwo.clother.security.vo.LoginRequestVO;
 import com.microsoftwo.clother.security.vo.LoginResponseVO;
+import com.microsoftwo.clother.security.vo.TokenRequestVO;
 import com.microsoftwo.clother.user.aggregate.UserEntity;
 import com.microsoftwo.clother.user.repository.UserRepository;
 import com.microsoftwo.clother.user.service.UserService;
@@ -79,5 +80,39 @@ public class AuthService {
                 .accessTokenExpiresIn(tokenDTO.getAccessTokenExpiresIn())
                 .userId(userEntity.getId())
                 .build();
+    }
+
+    @Transactional
+    public TokenDTO reissue(TokenRequestVO tokenRequestDto) {
+        // Refresh Token 검증
+        if (!tokenProvider.validateToken(tokenRequestDto.getRefreshToken())) {
+            throw new RuntimeException("Refresh Token 이 유효하지 않습니다.");
+        }
+
+        // Access Token 에서 Member ID 가져오기
+        Authentication authentication = tokenProvider.getAuthentication(tokenRequestDto.getAccessToken());
+
+        // 로그인한 유저의 정보 가져오기
+        UserEntity userEntity = userRepository.findByEmail(tokenRequestDto.getAccessToken())
+                .orElseThrow(() -> new RuntimeException("해당 유저를 찾을 수 없습니다."));
+
+        // 저장소에서 Member ID 를 기반으로 Refresh Token 값 가져옴
+        RefreshToken refreshToken = refreshTokenRepository.findByKey(authentication.getName())
+                .orElseThrow(() -> new RuntimeException("로그아웃 된 사용자입니다."));
+
+        // Refresh Token 일치하는지 검사
+        if (!refreshToken.getValue().equals(tokenRequestDto.getRefreshToken())) {
+            throw new RuntimeException("토큰의 유저 정보가 일치하지 않습니다.");
+        }
+
+        // 새로운 토큰 생성
+        TokenDTO tokenDto = tokenProvider.generateTokenDTO(authentication, userEntity.getId());
+
+        // 저장소 정보 업데이트
+        RefreshToken newRefreshToken = refreshToken.updateValue(tokenDto.getRefreshToken());
+        refreshTokenRepository.save(newRefreshToken);
+
+        // 토큰 발급
+        return tokenDto;
     }
 }
