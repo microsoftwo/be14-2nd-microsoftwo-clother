820c58bad0da18e348e5b065030e2e29a5118958
닉네임 < >
2025-04-03 00:27:32 +0900

    [refactor] feign 관련 코드 제거

--- core-service/src/main/java/com/microsoftwo/clother/Be142ndMicrosoftwoClotherApplication.java
+++ core-service/src/main/java/com/microsoftwo/clother/Be142ndMicrosoftwoClotherApplication.java
@@ -3,11 +3,9 @@ package com.microsoftwo.clother;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
-import org.springframework.cloud.openfeign.EnableFeignClients;
 
 @SpringBootApplication(scanBasePackages = "com.microsoftwo.clother")
 @EnableDiscoveryClient
-@EnableFeignClients
 public class Be142ndMicrosoftwoClotherApplication {
 
     public static void main(String[] args) {
